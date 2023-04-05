a2a64af722ecd9434f6930961f953241a4a9c362
Change Agent <keasiley@icloud.com>
2023-04-05 16:30:08 -0500

    index file edited with more inqire questions, shapes edited with a constructor

--- index.js
+++ index.js
@@ -9,21 +9,56 @@ function init() {
       {
         type: "list",
         message: "Would you like to create a logo?",
+        name: "logo",     
         choices: ["yes","no"],
 
       },
 
       {
         type: "input",
-        message: "What is the title of your project?",
-        name: "title",
+        message: "What color would you like to display your text?",
+        name: "text",
+      },
+
+      {
+        type: "list",
+        message: "What would you like for your first shape?",
+        name: "shape",
+        choices: ["Triangle, Circle, Square"],
       },
 
       {
         type: "input",
-        message: "Enter a description of your project",
-        name: "description",
+        message: "What color would like the color of your first shape to be?",
+        name: "text",
       },
+
+      {
+        type: "list",
+        message: "What would you like for your Second shape?",
+        name: "shape",
+        choices: ["Triangle, Circle, Square"]
+      },
+
+      {
+        type: "input",
+        message: "What color would like the color of your second shape to be?",
+        name: "text",
+      },
+
+      {
+        type: "list",
+        message: "What would you like for your third shape?",
+        name: "shape",
+        choices: ["Triangle, Circle, Square"]
+      },
+
+      {
+        type: "input",
+        message: "What color would like the color of your third shape to be?",
+        name: "text",
+      },
+
     
     ])
    

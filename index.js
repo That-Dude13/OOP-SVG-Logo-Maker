c962b1eebf8418ed03e5ccc9366f7b1793dd789a
Change Agent <keasiley@icloud.com>
2023-04-07 12:41:53 -0500

    Folder strcture changed for individual test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const shapes = require(".shapes.js");
+const shapes = require("./lib/shapes");
 const test = require("./lib/shapes.test")
 
 function init() {
